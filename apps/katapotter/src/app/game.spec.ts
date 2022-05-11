97f8f2c5e6aecefec508e8c231f267eb80eea532
aquila <aquilaw@gmail.com>
2022-05-11 14:31:24 +0800

    task2 RED

--- apps/katapotter/src/app/game.spec.ts
+++ apps/katapotter/src/app/game.spec.ts
@@ -7,4 +7,10 @@ describe('Game', () => {
     game.buy(1);
     expect(game.price).toBe(100);
   });
+
+  test('buy two', () => {
+    const game = new Game();
+    game.buy(2);
+    expect(game.price).toBe(190);
+  });
 });
