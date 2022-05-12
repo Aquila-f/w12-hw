62df303b0521ebcbd2a0c3a3d9a772a23e5994c6
aquila <aquilaw@gmail.com>
2022-05-12 12:42:26 +0800

    task5,6 GREEN

--- apps/katapotter/src/app/game.spec.ts
+++ apps/katapotter/src/app/game.spec.ts
@@ -26,4 +26,31 @@ describe('Game', () => {
     game.buy("p2", 2);
     expect(game.price).toBe(290);
   });
+
+  test('buy five', () => {
+    game.buy("p1", 1);
+    game.buy("p2", 1);
+    game.buy("p3", 1);
+    game.buy("p4", 1);
+    game.buy("p5", 1);
+    expect(game.price).toBe(375);
+  });
+
+  test('buy six', () => {
+    game.buy("p1", 1);
+    game.buy("p2", 1);
+    game.buy("p3", 2);
+    game.buy("p4", 1);
+    game.buy("p5", 1);
+    expect(game.price).toBe(475);
+  });
+
+  test('buy seven', () => {
+    game.buy("p1", 1);
+    game.buy("p2", 1);
+    game.buy("p3", 2);
+    game.buy("p4", 2);
+    game.buy("p5", 1);
+    expect(game.price).toBe(565);
+  });
 });
