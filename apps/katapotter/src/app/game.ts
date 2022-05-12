c336cb45dfdfbadab0d02623f93608581e36809e
aquila <aquilaw@gmail.com>
2022-05-12 12:25:33 +0800

    task3 GREEN (+REFACTOR)

--- apps/katapotter/src/app/game.ts
+++ apps/katapotter/src/app/game.ts
@@ -1,10 +1,11 @@
 export class Game {
-    // private _price = 0;
+    private total = 0;
     private booksvector: bookinfo[] = [];
-    private discount: number[] = [1, 0.95, 0.9, 0.8, 0.75];
+    private discount: number[] = [100, 190, 270, 320, 375];
 
     buy(bookname: string, num: number){
         let flag = false;
+        this.total += num;
         for( let i=0;i<this.booksvector.length;i++ ){
             if(this.booksvector[i].name == bookname){
                 flag = true;
@@ -13,20 +14,26 @@ export class Game {
         }
         if(flag == false){
             // let book = foo(bookname, num);
-            console.log("---------")
+            // console.log("---------")
             this.booksvector.push(foo(bookname, num))
         }
     }
     get price(){
         let final_price = 0;
-        console.log(this.booksvector.length)
-        for( let i=0;i<this.booksvector.length;i++ ){
-            final_price += this.booksvector[i].len * 100;
-            console.log(this.booksvector[i].len)
-            console.log(this.booksvector[i].name)
+        // console.log(this.booksvector.length)
+        let getbook = 0;
+        let p_ = 0;
+        for(let iter=0;iter<this.total;){
+            p_ = 0;
+            for(let i=0;i<this.booksvector.length;i++){
+                if(this.booksvector[i].len > getbook){
+                    p_ += 1;
+                    iter += 1;
+                }
+            }
+            final_price += this.discount[p_-1];
+            getbook += 1;
         }
-
-        final_price = final_price * this.discount[this.booksvector.length-1];
 
         return final_price;
     }
